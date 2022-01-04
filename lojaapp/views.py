0d691f8bb65d94148c1851abfd125e91d1c608ee
Antônio Elvis Pinheiro <52458608+Elvis0032@users.noreply.github.com>
2022-01-04 18:58:34 -0300

    Criação do carrinho de compras

--- lojaapp/views.py
+++ lojaapp/views.py
@@ -5,24 +5,56 @@ from.models import *
 
 class HomeView(TemplateView):
     template_name = "home.html"
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['produto_list'] = Produto.objects.all().order_by("-id")
         return context
 
-class  TodosProdutosView(TemplateView):
+
+class TodosProdutosView(TemplateView):
     template_name = "todosprodutos.html"
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['todascategorias'] = Categoria.objects.all()
-        return context      
+        return context
 
-class  ProdutoDetalheView(TemplateView):
+
+class ProdutoDetalheView(TemplateView):
     template_name = "produtodetalhe.html"
-   
-   
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        url_slug = self.kwargs['slug']
+        produto = Produto.objects.get(slug=url_slug)
+        produto.visualizacao += 1
+        produto.save()
+        context['produto'] = produto
+        return context
+
+
+class AddCarroView(TemplateView):
+    template_name = "addprocarro.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        produto_id = self.kwargs['pro_id']
+        produto_obj = Produto.objects.get(id=produto_id)
+        carro_id = self.request.session.get("carro_id", None)
+
+        if carro_id:
+
+            carro_obj = Carro.objects.get(id=carro_id)
+
+        else:
+            carro_obj = Carro.objects.create(total=0)
+            self.request.session['carro_id'] = carro_obj.id
+
+
 class SobreView(TemplateView):
     template_name = "sobre.html"
-    
+
+
 class ContatoView(TemplateView):
     template_name = "contato.html"
